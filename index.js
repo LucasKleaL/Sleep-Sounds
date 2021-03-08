02f7d0bdf29bc4446f66306058aa6dcb7627ff11
LucasKleaL <lucaskleal222@outlook.com>
2021-03-07 22:04:35 -0300

    Commit 3.2.5
    -Testes com o service worker.

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
     registerServiceWorker();
+
 })
 
 async function registerServiceWorker() { //responsavel por registrar e chamar o service worker 
     if ('serviceWorker' in navigator) {
         try{
             window.addEventListener("load", function() {
-                navigator.serviceWorker.getRegistrations().then((r)=>{r[0].unregister()})
-                navigator.serviceWorker.register("/sw.js");
+                navigator.serviceWorker.getRegistrations().then(function(registrations) { for(let registration of registrations) { registration.unregister() } })
+                navigator.serviceWorker.register("./sw.js");
             })
             console.log("Server Worker registrated.")
         }
