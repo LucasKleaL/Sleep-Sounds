4e7719b1fd497f3021f4d4dae690cb3f23901ae6
LucasKleaL <lucaskleal222@outlook.com>
2021-03-08 00:18:18 -0300

    4.1.1
    -Correção no nome de uma função.

--- sw.js
+++ sw.js
@@ -50,7 +50,7 @@ function fromCache(request) {
 
 function update(request) {
     return caches.open(cacheName).then(function (cache) {
-        return fetch(request).then(function(responce) {
+        return fetch(request).then(function(response) {
             return cache.put(request, response.clone()).then(function() {
                 return response;
             });
